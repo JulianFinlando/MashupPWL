f2b49fdc9dde30919eb8ca38a497fe17e7ffa725
Bhernardin Erryco <bhernardin.erryco@ti.ukdw.ac.id>
2018-09-15 16:38:34 +0700

    Search mobilBekas & Mobil88

--- templatePWL/search.php
+++ templatePWL/search.php
@@ -17,27 +17,65 @@
     <link href="css/shop-homepage.css" rel="stylesheet">
 
   </head>
-  <?php 
-  ini_set('display_errors', 'off');
-    $url = "http://www.mobil88.astra.co.id/";
-		$ch = curl_init();
 
-		//Jika POST
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$output = curl_exec($ch);
-		curl_close($ch);
-    $dom = NEW DOMDocument();
-		$dom -> loadHTML($output);
-    $xpath = NEW DOMXPath($dom);
+  <?php
+    ini_set('display_errors', 'off');
+    if(isset($_POST['keyword'])){
+        
+        $url="http://www.mobil88.astra.co.id/cari_mobil?keyword=".$_POST['keyword']."";
+        $ch = curl_init();
 
-      $resultsMobil88 = $xpath->query('//div[@class="cars grid-4 home"]/div[@class="fake-anchor"][1]');
-      
-		//echo "<h3>Hasil Pencarian untuk kata '".$_POST['keyword']."'</h3>";
-		foreach ($resultsMobil88 as $resultMobil88) {
-      //echo $result1->row[1]->childNodes[3]->nodeValue."<br/><br/>";
+        //Jika POST
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+        $dom = new DOMDocument();
+        $dom->loadHTML($output);
+        //echo $output;
+        $xpath = new DOMXPath($dom);
+        $results = $xpath->query('//div[@class="cars grid-3"]/div[@class="inline hoverable"]');
+
+        //mobilbekas
+        $url2="https://www.mobilbekas.co.id/harga-bekas/a-".$_POST['keyword']."/";
+        $ch2 = curl_init();
+
+        //Jika POST
+        curl_setopt($ch2, CURLOPT_URL, $url2);
+        curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+        $output2 = curl_exec($ch2);
+        curl_close($ch2);
+
+        $dom2 = new DOMDocument();
+        $dom2->loadHTML($output2);
+        //echo $output;
+        $xpath2 = new DOMXPath($dom2);
+        $results2 = $xpath2->query('//div[@class="search-content"]/div/div[@class="listing-block"]/a[@class="listing-url  "]/div[@class="listing-content"]');
+
+        $results2L = $xpath2->query('//div[@class="search-content"]/div/div[@class="listing-block"]');
+
+
+        //mobilwow
+        $url="http://www.mobil88.astra.co.id/cari_mobil?keyword=".$_POST['keyword']."";
+        $ch = curl_init();
+
+        //Jika POST
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+        $dom = new DOMDocument();
+        $dom->loadHTML($output);
+        //echo $output;
+        $xpath = new DOMXPath($dom);
+        $results = $xpath->query('//div[@class="cars grid-3"]/div[@class="inline hoverable"]');
+        
+        
+    }
     
-?>
+  ?>
 
   <body>
 
@@ -50,8 +88,8 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <form class="form-inline" action="/action_page.php">
-              <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <form class="form-inline" method="post" action="">
+              <input class="form-control mr-sm-2" type="text" name="keyword" placeholder="Search">
               <button class="btn btn-success" type="submit">Search</button>
             </form>
           </ul>
@@ -69,7 +107,7 @@
 
           <h2>Mobil 88</h2>
           <div class="row">
-
+          <?php foreach ($results as $result) { ?>
             
             
             <div class="col-lg-3 col-md-6 mb-4">
@@ -78,135 +116,44 @@
                 
                   <div class="card-body">
                   <h4 class="card-title">
-                    <a href="#"><?php echo $resultMobil88->childNodes[3]->nodeValue."<br/><br/>"; ?></a>
+                    <a href="#"><?php echo $result->childNodes[3]->nodeValue."<br/><br/>"; ?></a>
                     
                   </h4>
-                  <h5><?php echo $resultMobil88->childNodes[7]->nodeValue."<br/><br/>"; ?></h5>
-                  <p class="card-text"><?php echo $resultMobil88->childNodes[5]->nodeValue."<br/><br/>"; ?></p>
+                  <h5><?php echo $result->childNodes[5]->nodeValue."<br/><br/>";?></h5>
+                  <p class="card-text"></p>
                 </div>
                 <div class="card-footer">
                   <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
                 </div>
               </div>
             </div>
-                
-
-            <div class="col-lg-3 col-md-6 mb-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-body">
-                  <h4 class="card-title">
-                    <a href="#"></a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-                </div>
-                <div class="card-footer">
-                  <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 mb-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-body">
-                  <h4 class="card-title">
-                    <a href="#"></a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div class="card-footer">
-                  <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 mb-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-body">
-                  <h4 class="card-title">
-                    <a href="#"></a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div class="card-footer">
-                  <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
+            <?php } ?>
 
           </div>
-    <?php } ?>
-          <h2>Mobil 123</h2>
+
+          <h2>Mobil Bekas</h2>
           <div class="row">
+            <?php foreach ($results2L as $result2L) { ?>
+            <?php foreach ($results2 as $result2) { ?>
 
             <div class="col-lg-3 col-md-6 mb-4">
               <div class="card h-100">
                 <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
                 <div class="card-body">
                   <h4 class="card-title">
-                    <a href="#">Item Five</a>
+                    <a href="<?php echo $result2L->childNodes[1]->attributes['href']->nodeValue; ?>"><?php echo $result2->childNodes[1]->nodeValue."<br/><br/>";?></a>
                   </h4>
-                  <h5>$24.99</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+                  <h5><?php echo $result2->childNodes[3]->nodeValue."<br/><br/>";?></h5>
+                  <p class="card-text"></p>
                 </div>
                 <div class="card-footer">
                   <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
                 </div>
               </div>
             </div>
-
-            <div class="col-lg-3 col-md-6 mb-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-body">
-                  <h4 class="card-title">
-                    <a href="#">Item Six</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div class="card-footer">
-                  <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 mb-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-body">
-                  <h4 class="card-title">
-                    <a href="#">Item Six</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div class="card-footer">
-                  <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 mb-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-body">
-                  <h4 class="card-title">
-                    <a href="#">Item Six</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div class="card-footer">
-                  <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
+            <?php } ?>
+            <?php } ?>
+            
 
           </div>
 
@@ -229,53 +176,6 @@
               </div>
             </div>
 
-            <div class="col-lg-3 col-md-6 mb-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-body">
-                  <h4 class="card-title">
-                    <a href="#">Item Six</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div class="card-footer">
-                  <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 mb-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-body">
-                  <h4 class="card-title">
-                    <a href="#">Item Six</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div class="card-footer">
-                  <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 mb-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-                <div class="card-body">
-                  <h4 class="card-title">
-                    <a href="#">Item Six</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div class="card-footer">
-                  <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
 
           </div>
           <!-- /.row -->
