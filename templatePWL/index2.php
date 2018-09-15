ef751ae78c16359c5c82daad619ef5e6950673d5
Bhernardin Erryco <bhernardin.erryco@ti.ukdw.ac.id>
2018-09-15 14:21:29 +0700

    Ubah Search

--- templatePWL/index2.php
+++ templatePWL/index2.php
@@ -87,9 +87,9 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <form class="form-inline" action="/action_page.php">
-              <input class="form-control mr-sm-2" type="text" placeholder="Search">
-              <button class="btn btn-success" type="submit">Search</button>
+            <form class="form-inline" action="search.php">
+              <!--<input class="form-control mr-sm-2" type="text" placeholder="Search">-->
+              <button class="btn btn-success" type="submit">Cari Mobil</button>
             </form>
           </ul>
         </div>
